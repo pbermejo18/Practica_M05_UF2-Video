0f145b79eff4a732fb8cb993d8357171003e3748
Pol <pbermejo@elpuig.xeill.net>
2022-01-23 20:47:39 +0100

    Branch1-metodo

---
+++ src/Metodo.java
@@ -0,0 +1,13 @@
+// Ultima parte del video JAVADOC
+
+public class Metodo {
+    // Ultima parte del video JAVADOC
+
+    String uno = "Pol";
+    int years = 19;
+    String dos = "años";
+
+    void Txt(String uno, int years, String dos){
+        System.out.println(uno + " tiene " + years + " " + dos);
+    }
+}
