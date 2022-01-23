848c886b20f029379fee556d89f6b97cf6080c25
Pol <pbermejo@elpuig.xeill.net>
2022-01-23 20:12:45 +0100

    Branch1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("1");
+        System.out.println("Hello Branch1");
     }
 }
